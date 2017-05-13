7c44a52912256f93eff0de64baa1475bd36fa98e
samir.hashem10 <samir.hashem10@gmail.com>
2017-05-13 04:11:27 -0500

    initial commit

---
+++ ProjectDowntownInventory/ProjectDowntownInventory/Default.aspx
@@ -0,0 +1,96 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ProjectDowntownInventory.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script type="text/javascript" src="http://cdn.jsdelivr.net/json2/0.1/json2.js"></script>
+    <script type="text/javascript">
+    $(function () {
+        $("[id*=SubmitItemAdd]").bind("click", function () {
+            var it = {};
+            it.ITEM = $("[id*=ItemAddTxt]").val();
+            $.ajax({
+                type: "POST",
+                url: "Default.aspx/addInventory",
+                data: '{it: ' + JSON.stringify(i) + '}',
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (response) {
+                    alert("Item has been added successfully.");
+                    window.location.reload();
+                }
+            });
+            return false;
+        });
+    });
+</script>
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<div id="Auth" runat="server" class="col-lg-8 col-lg-offset-2">
+<table border="0" cellpadding="0" cellspacing="0">
+    <tr>
+        <td>
+            Username:
+        </td>
+        <td>
+            <asp:TextBox ID="txtUsername" runat="server" Text="" CssClass="form-control input-lg " />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Password:
+        </td>
+        <td>
+            <asp:TextBox ID="txtPassword" runat="server" TextMode="Password" CssClass="form-control input-lg" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+        </td>
+        <td>
+            <asp:Button ID="SubmitButton" Text="Submit" runat="server" OnClick="SubmitButton_Click" CssClass="form-control btn-block"/>
+        </td>
+    </tr>
+</table>
+<hr />
+</div>
+<div id="tbl" runat="server" class ="col-lg-8 col-lg-offset-2">
+
+    <div class="col-lg-8 col-lg-offset-2">
+        <asp:TextBox ID="ItemAddTxt" runat="server" CssClass ="form-control input-sm col-lg-10"></asp:TextBox>
+        <asp:Button ID="SubmitItemAdd" Text="Submit Item" runat="server" CssClass="form-control btn-default" OnClick="SubmitItemAdd_Click"/>
+        <div id="SuccessMessage" runat="server" visible="false"><p>Item added Successfully.</p></div>
+    </div>
+    <br />
+    <br />
+    <div>
+    <asp:GridView ID="gvUsers" runat="server" AutoGenerateColumns="false" CssClass="table table-hover table-striped" OnRowDeleting="gvUsers_RowDeleting" >
+    <Columns>
+        <asp:BoundField DataField="ID" HeaderText="ID"/>
+        <asp:BoundField DataField="ITEM" HeaderText="ITEM" />
+        <asp:TemplateField HeaderText="QUANTITY ON HAND">
+            <ItemTemplate>
+                <asp:TextBox ID="txtRowQTY" runat="server" Text='<%# Eval("QUANTITY") %>' />
+                <asp:RegularExpressionValidator ID="RegularExpressionValidator1" ControlToValidate="txtRowQTY" runat="server" ErrorMessage="Only Numbers allowed" ValidationExpression="\d+"></asp:RegularExpressionValidator>
+            </ItemTemplate>
+         </asp:TemplateField>
+        <asp:TemplateField HeaderText="UNITS">
+            <ItemTemplate>
+                <asp:TextBox ID="txtRowUNITS" runat="server" Text='<%# Eval("UNITS") %>' />
+            </ItemTemplate>
+         </asp:TemplateField>
+        <asp:TemplateField HeaderText="ORDER MORE">
+            <ItemTemplate>
+                <asp:CheckBox ID="chkRow" runat="server" Checked='<%# Eval("ORDER_MORE") %>' />
+            </ItemTemplate>
+         </asp:TemplateField>
+        <asp:CommandField ShowDeleteButton="True" ButtonType="Button" />
+
+    </Columns>
+    </asp:GridView>
+        <div id="SaveSuccessMsg" runat="server" visible="false"><p>Changes Saved...</p></div>
+        <asp:Button ID="btnSaveChanges" Text="Save Changes" runat="server" CssClass="form-control btn-default" OnClick="btnSaveChanges_Click"/>
+
+    </div>
+</div>
+</asp:Content>
---
+++ ProjectDowntownInventory/ProjectDowntownInventory/Default.aspx.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services;
+using System.Web.Script.Services;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProjectDowntownInventory
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                tbl.Visible = false;
+                string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    using (SqlCommand cmd = new SqlCommand("SELECT * FROM I_Inventory"))
+                    {
+                        using (SqlDataAdapter sda = new SqlDataAdapter())
+                        {
+                            DataTable dt = new DataTable();
+                            cmd.CommandType = CommandType.Text;
+                            cmd.Connection = con;
+                            sda.SelectCommand = cmd;
+                            sda.Fill(dt);
+                            gvUsers.DataSource = dt;
+                            gvUsers.DataBind();
+                        }
+                    }
+                }
+            }
+            if(Session["Auth"] == (object)"1")
+            {
+                tbl.Visible = true;
+                Auth.Visible = false;
+            }
+        }
+        [WebMethod]
+        [ScriptMethod]
+        public static void addInventory(AddItem it)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("EXEC P_ADD_ITEM @ITEM"))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@ITEM", it.ITEM);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+        }
+        public class AddItem
+        {
+            public string ITEM { get; set; }
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            var _auth = "";
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["constr"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("EXEC P_AUTH_USER @Username, @Password", con))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                    cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+                    using (SqlDataAdapter adap = new SqlDataAdapter(cmd))
+                    {
+                        var dt = new DataTable();
+                        adap.Fill(dt);
+                        _auth = dt.Rows[0][0].ToString();
+                    }
+                    if(_auth == "1")
+                    {
+                        Auth.Visible = false;
+                        tbl.Visible = true;
+                        Session["Auth"] = (object)"1";
+                    }
+                }
+            }
+        }
+
+        protected void SubmitItemAdd_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["constr"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("EXEC P_ADD_ITEM @ITEM", con))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@ITEM", ItemAddTxt.Text);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    Response.Redirect(Request.RawUrl);
+
+                }
+            }
+        }
+
+        protected void gvUsers_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["constr"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("EXEC P_DELETE_ITEM @ID", con))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add("@ID", SqlDbType.Int);
+                    var te = gvUsers.Rows[e.RowIndex].Cells[0].Text;
+                    cmd.Parameters["@ID"].Value = te;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    Response.Redirect(Request.RawUrl);
+                }
+            }
+        }
+
+        protected void btnSaveChanges_Click(object sender, EventArgs e)
+        {
+            foreach(GridViewRow row in gvUsers.Rows)
+            {
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["constr"].ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("EXEC P_EDIT_ITEM @ID, @QUANTITY, @UNITS,  @ORDER_MORE", con))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Parameters.Add("@ID", SqlDbType.Int);
+                        string id = row.Cells[0].Text;
+                        cmd.Parameters["@ID"].Value = id;
+                        TextBox qty = (row.Cells[2].FindControl("txtRowQTY") as TextBox);
+                        cmd.Parameters.AddWithValue("@QUANTITY", qty.Text);
+                        TextBox unt = (row.Cells[3].FindControl("txtRowUNITS") as TextBox);
+                        cmd.Parameters.AddWithValue("@UNITS", unt.Text);
+                        cmd.Parameters.Add("@ORDER_MORE", SqlDbType.Int);
+                        CheckBox chkRow = (row.Cells[4].FindControl("chkRow") as CheckBox);
+                        int te = 0;
+                        if(chkRow.Checked) { te = 1; }
+                        cmd.Parameters["@ORDER_MORE"].Value = te;
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+            }
+            SaveSuccessMsg.Visible = true;
+            Response.Redirect(Request.RawUrl);
+
+        }
+    }
+}
